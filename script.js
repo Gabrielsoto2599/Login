42478a9450851eaf1a21df1fe85148f0ac353d5f
Gabrielsoto2599 <135544457+Gabrielsoto2599@users.noreply.github.com>
2026-02-03 23:17:51 -0400

    script.js
    Correcciones del script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const submSingIn = document.getElemtentById("sing-in");
-      submSingUp = document.getElemtentById("sing-up");
-      formRegister = document.querySelector(".register");
-      formLogin = document.querySelector(".login");
+const btnSignIn = document.getElementById("sing-in"); // Corregido: getElementById
+const btnSignUp = document.getElementById("sing-up"); // Corregido: getElementById
+const formRegister = document.querySelector(".register");
+const formLogin = document.querySelector(".login");
 
-
-submSingIn.addEventListener("click", e => {
+btnSignIn.addEventListener("click", e => {
+     e.preventDefault(); // Muy importante para que no se recargue la página
      formRegister.classList.add("hide");
-     formLogin.classList.remove("hide")
-})
+     formLogin.classList.remove("hide");
+});
 
-submSingUp.addEventListener("click", e => {
+btnSignUp.addEventListener("click", e => {
+     e.preventDefault(); // Muy importante para que no se recargue la página
      formLogin.classList.add("hide");
-     formRegister.classList.remove("hide")
+     formRegister.classList.remove("hide");
+}); // Corregido: Cierre de llaves y paréntesis
